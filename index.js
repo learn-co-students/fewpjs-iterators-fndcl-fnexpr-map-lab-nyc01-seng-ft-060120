53f165fe6cb1900bde6cb6b2cf2b5f4f9b392d32
dr-dolce14 <dolcemascop1@gmail.com>
2020-07-22 09:11:40 -0400

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map( line => {
+    let words = line.split(' ')
+    let capitalizedWords =
+    words.map(word => word.charAt(0).toUpperCase() + word.slice(1) )
+    let result = capitalizedWords.join(' ')
+    return result
+  })
 }
