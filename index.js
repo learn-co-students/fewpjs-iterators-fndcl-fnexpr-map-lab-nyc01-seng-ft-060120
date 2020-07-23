62c7908fd71f37290d4952766437c2896265f98c
travisprol0 <travisjohnprol@gmail.com>
2020-07-23 13:25:45 -0400

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,10 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((title) => {
+    let new_title = title.split(" ").map((word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1)
+    });
+    return new_title.join(" ")
+  })
 }
